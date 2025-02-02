1ebbbfbd9d75c3b82806d65fc9e13d9dee785e18
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-02-01 20:38:39 -0500

    Create a memory class
    The emulator can store volatile data in an object of this type.

---
+++ Memory.cs
@@ -0,0 +1,44 @@
+/*
+ * The emulator's volatile memory representing the CHIP-8's RAM.
+ */
+public class Memory {
+    uint numBytes;
+    byte[] mem;
+
+    /*
+     * The size of the memory, in bytes.
+     */
+    public uint Size { get => numBytes; }
+
+    /*
+     * Constructs the memory object.
+     *
+     * Parameter:
+     *   size: The number of bytes that the memory can store.
+     */
+    public Memory(uint size) {
+	numBytes = size;
+        mem = new byte[size];
+    }
+
+    /*
+     * Set a memory address to a given byte.
+     *
+     * Parameters:
+     *   addr: The memory address to set to.
+     *   data: The byte to set.
+     */
+    public void Set(uint addr, byte data) {
+        mem[addr] = data;
+    }
+
+    /*
+     * Get the byte at a memory address.
+     *
+     * Parameter:
+     *   addr: The memory address to get from.
+     */
+    public byte Get(uint addr) {
+        return mem[addr];
+    }
+}
