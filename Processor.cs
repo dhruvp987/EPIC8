4dd25bbc3a7767ca9737df092837f7f51496f0cc
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-02-21 18:36:53 -0500

    Initial Processor class
    The Processor class will be responsible for executing CHIP-8
    instructions

---
+++ Processor.cs
@@ -0,0 +1,19 @@
+/*
+ * A processor that can execute CHIP-8 instructions.
+ */
+public class Processor {
+    const int NUM_GEN_REGS = 16;
+    const int PC_INDEX = NUM_GEN_REGS;
+    const int I_REG_INDEX = PC_INDEX + 1;
+
+    Registers regs;
+
+    /*
+     * Create a new processor.
+     */
+    public Processor() {
+	// Create the general registers plus the program counter
+	// and index register.
+        regs = new Registers(NUM_GEN_REGS + 2);
+    }
+}
